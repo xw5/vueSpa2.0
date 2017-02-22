75eff1a0c3eb0596bc38b00a0e77c1b3260cbdc7
xiewu <xiewu@meizu.com>
2017-02-22 19:25:08 +0800

    vueSpa2.0示例项目备份

---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import App from './App'
+import Home from './components/Home'
+import 'bootstrap/dist/css/bootstrap.css'
+
+Vue.use(VueRouter);
+
+const routes=[{
+    path:'/',
+    component:Home
+  },{
+    path:'/home',
+    component:Home
+  }
+]
+
+const router=new VueRouter({
+  routes
+})
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  ...App
+})
